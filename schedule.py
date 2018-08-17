c275dbf3167a1cf0cdba1b3a7f0cad2555a04206
Charlie <32808601+Charliesgithub20221030@users.noreply.github.com>
2018-08-17 22:08:06 +0800

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -6,9 +6,9 @@ def job():
 
 
 
-if __name__=='__main__':
-    second_5_j = schedule.every(5).seconds.do(job)
-  # 無窮迴圈
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+
+second_5_j = schedule.every(5).seconds.do(job)
+# 無窮迴圈
+while True:
+    schedule.run_pending()
+    time.sleep(1)
