98f44aafa6da6162486cbe6e12d9a8387efdb715
Charlie <32808601+Charliesgithub20221030@users.noreply.github.com>
2018-08-17 21:51:11 +0800

    Add files via upload

---
+++ schedule.py
@@ -0,0 +1,13 @@
+import schedule
+import time
+
+def job():
+    print("I'm working...")
+
+second_5_j = schedule.every(5).seconds.do(job)
+
+
+# 無窮迴圈
+while True:
+    schedule.run_pending()
+    time.sleep(1)
